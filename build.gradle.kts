4e3e03b8911192dcad2ab43e41080b837628547f
Benjie Cheong <benjie.cheong@gmail.com>
2021-09-28 15:35:56 +0800

    Added spring cloud gateway dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 repositories {
@@ -7,7 +8,15 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
     testImplementation(kotlin("test"))
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2020.0.3")
+    }
 }
 
 tasks {
