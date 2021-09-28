bf3d8c3f66a409c8806f4541bc92620faf6da36f
Benjie Cheong <benjie.cheong@gmail.com>
2021-09-28 16:29:34 +0800

    Added spring boot dependencies and basic setup

---
+++ src/main/kotlin/io/gateway/server/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package io.gateway.server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(GatewayApplication::class.java, *args)
+}
